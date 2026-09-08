a2d0f8cc1a6f28adaef5ceb6e854087e118a2965
Mahi-Haque <sheikhmahidul37@gmail.com>
2026-09-09 00:18:26 +0600

    Discuss datatypes including null is object

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accoundId = 144553;
+let accoundEmail = "mahi@google.com";
+var accountPassword = "12345";
+acccountCity = "Dhaka";
+let accountState;
+// accoundId = 2; not allowed
+
+accoundEmail = "mahidul@gamil.com";
+accountPassword = "45326";
+acccountCity = "Khulna";
+
+console.log(accoundId);
+
+console.table([
+  accoundEmail,
+  accoundId,
+  accountPassword,
+  acccountCity,
+  accountState,
+]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use  strict"; // Treat all JS code as newer version
+
+// alert (3 + 3) // we are using nodejs, not browser.
+
+console.log(3 + 3);
+
+console.log("Mahidul");
+
+let name = "Mahidul";
+let age = 18;
+let isLogggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean  => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// Object
+
+console.log(typeof age); // undefined
+console.log(typeof age); // Object
